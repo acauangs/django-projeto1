28ce1683b373339a890855a6272e6b6d866d3f19
Acauan Gomes <acauan_gomes@hotmail.com>
2022-03-15 17:16:58 -0300

    Category-Commit-Ok

--- recipes/admin.py
+++ recipes/admin.py
@@ -2,16 +2,14 @@ from django.contrib import admin
 
 from .models import Category, Recipe
 
-# Register your models here.
-
 
 class CategoryAdmin(admin.ModelAdmin):
     ...
 
 
+@admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     ...
 
 
-admin.site.register(Category, CategoryAdmin),
-admin.site.register(Recipe, RecipeAdmin),
+admin.site.register(Category, CategoryAdmin)
--- recipes/models.py
+++ recipes/models.py
@@ -1,11 +1,9 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-# MyModels.
-
 
 class Category(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=65)
 
     def __str__(self):
         return self.name
@@ -24,10 +22,14 @@ class Recipe(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    cover = models.ImageField(
+        upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True)
-    autor = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+        Category, on_delete=models.SET_NULL, null=True
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True
+    )
 
     def __str__(self):
         return self.title
