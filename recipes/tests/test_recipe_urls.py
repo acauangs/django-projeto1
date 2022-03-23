314d17b577c7fe433ab1f8a39c94d3ae395413c5
Acauan Gomes <acauan_gomes@hotmail.com>
2022-03-23 18:04:32 -0300

    Pasta-Test-MaisTest

---
+++ recipes/tests/test_recipe_urls.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+from django.urls import reverse  # teste ao contrario da URL
+
+
+class RecipeURLsTest(TestCase):
+    # Teste de Urls #
+    def test_recipe_home_url_is_correct(self):
+        url = reverse('recipes:home')
+        self.assertEqual(url, '/')
+
+    def test_recipe_category_url_is_correct(self):
+        url = reverse('recipes:category', kwargs={'category_id': 1})
+        self.assertEqual(url, '/recipes/category/1/')
+
+    def test_recipe_detail_url_is_correct(self):
+        url = reverse('recipes:recipe', kwargs={'id': 1})
+        self.assertEqual(url, '/recipes/1/')
+
+    def test_recipe_home_view_returns_status_code_200_OK(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_home_view_loads_correct_template(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertTemplateUsed(response, 'recipes/pages/home.html')
