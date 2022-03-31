ddd01575a42f4bd5afb8aece746a07f4edf81398
Acauan Gomes <acauan_gomes@hotmail.com>
2022-03-30 23:40:14 -0300

    Models-Test-CoverageInstall

--- recipes/tests/test_recipe_urls.py
+++ recipes/tests/test_recipe_urls.py
@@ -1,9 +1,8 @@
 from django.test import TestCase
-from django.urls import reverse  # teste ao contrario da URL
+from django.urls import reverse
 
 
 class RecipeURLsTest(TestCase):
-    # Teste de Urls #
     def test_recipe_home_url_is_correct(self):
         url = reverse('recipes:home')
         self.assertEqual(url, '/')
@@ -15,11 +14,3 @@ class RecipeURLsTest(TestCase):
     def test_recipe_detail_url_is_correct(self):
         url = reverse('recipes:recipe', kwargs={'id': 1})
         self.assertEqual(url, '/recipes/1/')
-
-    def test_recipe_home_view_returns_status_code_200_OK(self):
-        response = self.client.get(reverse('recipes:home'))
-        self.assertEqual(response.status_code, 200)
-
-    def test_recipe_home_view_loads_correct_template(self):
-        response = self.client.get(reverse('recipes:home'))
-        self.assertTemplateUsed(response, 'recipes/pages/home.html')
